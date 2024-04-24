280cecb26ac603c19310c8eba7574f5945edc6fa
Denis Zhitnyakov <dz@dolphin-software.online>
2024-04-24 16:00:08 +0300

    init

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function processLineByLine() {
   
   let processedFiles = 0;
   for (const file of files) {
-    if(processedFiles === 100) break;  
+    // if(processedFiles === 100) break;  
     const fileStream = fs.createReadStream(`../backend/unpacked/${file}`);
     // const logFormat = '$http_x_forwarded_for - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"';
     // const parseLine = nginxLogParser(logFormat);
